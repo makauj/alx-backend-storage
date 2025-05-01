c093129cc01c84dba57c8842267769d1f64d83f3
makau6 <makauwanyoike@gmail.com>
2025-05-01 22:56:55 +0300

    refactor: format AddBonus procedure for improved readability

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -2,9 +2,16 @@
 --that computes and store the average score for a student.
 DELIMITER $$
 
-CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT )
+CREATE PROCEDURE AddBonus(
+    IN user_id INT,
+    IN project_name VARCHAR(255),
+    IN score INT
+)
 BEGIN
-	IF NOT EXISTS(SELECT name FROM projects WHERE name=project_name) THEN
+	IF NOT EXISTS(
+        SELECT name FROM projects
+        WHERE name=project_name
+    ) THEN
 		INSERT INTO projects (name) VALUES (project_name);
 	END IF;
 
