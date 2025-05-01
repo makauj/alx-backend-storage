9deb0219ec2ac9aa3a3d967d6670279eeb6598cf
makau6 <makauwanyoike@gmail.com>
2025-05-01 22:33:55 +0300

    meetings

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -6,5 +6,4 @@ CREATE VIEW need_meeting AS
 SELECT name
 FROM students
 WHERE score < 80 AND
-(last_meeting IS NULL OR
-last_meeting < DATE(CURDATE() - INTERVAL 1 MONTH)));
+(last_meeting IS NULL OR last_meeting < DATE(CURDATE() - INTERVAL 1 MONTH));
