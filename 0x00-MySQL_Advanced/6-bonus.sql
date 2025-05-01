6972c9951d890d7cef2d3971835c376c32210def
makau6 <makauwanyoike@gmail.com>
2025-05-01 22:10:25 +0300

    some corrections

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,23 +1,18 @@
 -- SQL script that creates a stored procedure 'AddBonus' that adds a new correction for a student.
 -- The procedure takes the following parameters:user_id, project_name, score
 -- The procedure checks if the user exists in the database and if the project exists.
-DELIMITER $$
-
-CREATE PROCEDURE Addbonus (
-    IN user_id INT,
-    IN project_name VARCHAR(255),
-    IN score INT
+DELIMITER //
+CREATE PROCEDURE AddBonus(
+	IN user_id INTEGER,
+	IN project_name VARCHAR(255),
+	IN score INTEGER
 )
 BEGIN
     INSERT INTO projects (name)
-    SELECT project_name FROM DUAL
-    WHERE NOT EXISTS (
-        SELECT 1
-        FROM projects
-        WHERE name = project_name
-    );
-    INSERT INTO corrections (user_id, project_id, score)
-    SELECT(user_id, (SELECT id FROM projects WHERE name = project_name), score);
-END$$
+    SELECT project_name from DUAL 
+    WHERE NOT EXISTS (SELECT * FROM projects WHERE name=project_name LIMIT 1);
 
+    INSERT INTO corrections (user_id, project_id, score)
+    VALUES(user_id, (SELECT id from projects WHERE name=project_name), score);
+END //
 DELIMITER ;
--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,23 +1,12 @@
 -- SQL script that creates a stored procedure ComputeAverageScoreForUser
 --that computes and store the average score for a student.
 --Note: An average score can be a decimal
-DELIMITER $$
-
-CREATE PROCEDURE ComputeAverageScoreForUser (
-    IN user_id INT
-)
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+DELIMITER //
+CREATE PROCEDURE ComputeAverageScoreForUser(
+  IN user_id INT)
 BEGIN
-    DECLARE avg_score DECIMAL(5,2);
-
-    -- Calculate the average score for the user
-    SELECT AVG(score) INTO avg_score
-    FROM corrections
-    WHERE user_id = user_id;
-
-    
-    UPDATE users
-    SET average_score = avg_score
-    WHERE id = user_id;
-END$$
-
-DELIMITER ;
+  DECLARE avg_score FLOAT;
+  SET avg_score = (SELECT AVG(score) FROM corrections AS C WHERE C.user_id=user_id);
+  UPDATE users SET average_score = avg_score WHERE id=user_id;
+END; //
