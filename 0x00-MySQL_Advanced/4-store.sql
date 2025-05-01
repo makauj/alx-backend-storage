3c3e4ea02b95b0d43e22882621efc2b2d7e20cf4
makau6 <makauwanyoike@gmail.com>
2025-05-01 21:36:46 +0300

    store

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -3,16 +3,6 @@
 CREATE TRIGGER decrease_item_quantity
 AFTER INSERT ON orders
 FOR EACH ROW
-BEGIN
-    DECLARE item_id INT;
-    DECLARE item_quantity INT;
-
-    -- Get the item ID from the new order
-    SET item_id = NEW.item_id;
-
-    -- Get the current quantity of the item
-    SELECT quantity INTO item_quantity FROM items WHERE id = item_id;
-
-    -- Decrease the quantity of the item by 1
-    UPDATE items SET quantity = item_quantity - 1 WHERE id = item_id;
-END;
+UPDATE items
+SET quantity = quantity - NEW.number
+WHERE item_id = NEW.item_name;
