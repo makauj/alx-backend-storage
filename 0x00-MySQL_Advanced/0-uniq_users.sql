3170825eac72d3859cf2fb100145934ec968c957
makau6 <makauwanyoike@gmail.com>
2025-05-01 20:59:43 +0300

    UNIQ USERS

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,6 @@
+-- SQL script that creates a table named "users"
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL
+);
