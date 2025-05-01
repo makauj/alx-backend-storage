938d15eecc52f89e9b56c75c6f44150601b38437
makau6 <makauwanyoike@gmail.com>
2025-05-01 22:59:46 +0300

    refactor: rename procedure and update logic to compute average score for user

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -2,21 +2,21 @@
 --that computes and store the average score for a student.
 DELIMITER $$
 
-CREATE PROCEDURE AddBonus(
-    IN user_id INT,
-    IN project_name VARCHAR(255),
-    IN score INT
-)
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
-	IF NOT EXISTS(
-        SELECT name FROM projects
-        WHERE name=project_name
-    ) THEN
-		INSERT INTO projects (name) VALUES (project_name);
-	END IF;
+    DECLARE average_score FLOAT;
 
-	INSERT INTO corrections (user_id, project_id, score)
-	VALUES (user_id, (SELECT id from projects WHERE name=project_name), score);
+    SELECT AVG(score) INTO average_score
+    FROM corrections
+    WHERE user_id = user_id;
+
+    IF average_score IS NULL THEN
+        SET average_score = 0;
+    END IF;
+
+    UPDATE users
+    SET average_score = average_score
+    WHERE id = user_id;
 END$$
 
 DELIMITER ;
