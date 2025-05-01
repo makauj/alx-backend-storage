332096456250553c3d54f76017fb0b72cbcbb7ab
makau6 <makauwanyoike@gmail.com>
2025-05-01 21:14:11 +0300

    fans

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,7 @@
+-- SQL script that ranks country origins of bands, ordered by
+-- the number of (non-unique) fans
+
+SELECT origin, SUM(fans) AS fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY fans DESC;
